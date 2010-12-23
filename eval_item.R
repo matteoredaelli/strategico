85c2be02b04b2d271588735c14ef15d081d90c99
matteo.redaelli <matteo.redaelli@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2010-12-23 20:32:09 +0000

    initial import

---
+++ eval_item.R
@@ -0,0 +1,27 @@
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+library(methods)
+source("Rengine.R")
+
+args <- commandArgs()
+
+project.path <- args[6]
+item.path <- args[7]
+item.values <- args[8]
+item.params <- args[9]
+
+msg <- paste("Project=", project.path, ", Item=", item.path, sep="")
+print(msg)
+
+project.eval_item(project.path=project.path, pathToItem=item.path, values=item.values, param=item.params)                                          
