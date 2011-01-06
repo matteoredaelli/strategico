06767f4fad5adef13dba3f30afa5ffd5877a81cd
matteo.redaelli <matteo.redaelli@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2011-01-06 10:10:46 +0000

    added scripts for running the engine outside R, usefull for batch scheduling and web consoles

---
+++ eval_item.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 3 ]
+then
+  echo "Usage: `basename $0` project_path item_path value [params]"
+  exit 1
+fi
+
+project_path=$1
+item_path=$2
+value=$3
+params=$4
+
+script_path=/apps/strategico
+
+cd $script_path
+	
+command="${script_path}/eval_item.Rscript ${project_path} ${item_path} ${value} \"${params}\""
+echo "Running ${command}"
+
+/apps/R/bin/Rscript $command
+
---
+++ eval_items_slurm.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 3 ]
+then
+  echo "Usage: `basename $0` project_path item_path value [params]"
+  exit 1
+fi
+
+project_path=$1
+item_path=$2
+value=$3
+params=$4
+
+user=$(/usr/bin/whoami)
+
+script_path=${STRATEGICO_SCRIPTS}
+
+project_name=$(basename $project_path)
+
+cd $project_path/$item_path
+for file in $(find . -maxdepth 2 -mindepth 2 -name item.Rdata) ; do
+	cd $script_path
+	file_path=$(dirname $file)
+	new_item_path=$(echo ${item_path}/${file_path} | sed -e 's://:/:g' -e "s:\./::g")
+	all_params="${project_path} ${new_item_path} ${value} \"${params}\""
+	echo "Running params ${all_params}"
+	if [ "${user}" == "r" ] ; then
+		sbatch --workdir=/tmp --job-name=${project_name}/${new_item_path} ${script_path}/eval_items.sh ${all_params}
+	else
+		sudo sbatch --uid=1001 --gid=1001 --job-name=${project_name}/${new_item_path} --workdir=/tmp ${script_path}/eval_items.sh ${all_params}
+	fi
+done
+
