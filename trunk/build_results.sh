f25ecbd993143381afe3e8e8e8c3a35048fb321b
matteo.redaelli <matteo.redaelli@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2011-01-21 09:07:39 +0000

    only on script for collecting summaries and results

--- trunk/build_results.sh
+++ trunk/build_results.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
-  echo "Usage: `basename $0` projectPath valeName"
+  echo "Usage: `basename $0` projectPath results|summary valueName"
   exit 1
 fi
 
 project_path=$1
-value=$2
+report=$2
+value=$3
 
-find $project_path -name "*-${value}-results.csv" -exec cat {} \; > ${project_path}/${value}-results.xls
+find $project_path -name "report-${value}" -type d -maxdepth 2 -mindepth 2  -exec tail -1 {}/item-${report}.csv \; > ${project_path}/report-${value}/children-${report}.csv
+
