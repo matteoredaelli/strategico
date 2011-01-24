6a1552b58477f1e661a0c2b2215bcd3c4a63d455
matteo.redaelli <matteo.redaelli@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2011-01-24 14:02:01 +0000

    max depth must be specified

--- trunk/eval_items.sh
+++ trunk/eval_items.sh
@@ -2,19 +2,20 @@
 
 if [ $# -lt 3 ]
 then
-  echo "Usage: `basename $0` project_path item_path value [params]"
+  echo "Usage: `basename $0` project_path item_path depth value [params]"
   exit 1
 fi
 
 project_path=$1
 item_path=$2
-value=$3
-params=$4
+maxdepth=$3
+value=$4
+params=$5
 
 script_path=${STRATEGICO_SCRIPTS}
 
 cd $project_path/$item_path
-for file in $(find . -name item.Rdata) ; do
+for file in $(find . -name item.Rdata  -maxdepth ${maxdepth} -mindepth 1) ; do
 	cd $script_path
 	file_path=$(dirname $file)
 	new_item_path=$(echo ${item_path}/${file_path} | sed -e 's://:/:g' -e "s:\./::g")
