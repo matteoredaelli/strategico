ea6539f221a2aed699e89f10dccf4deb58595ff6
matteo.redaelli <matteo.redaelli@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2011-01-24 15:45:27 +0000

    lost name in html form

--- trunk/build_results.sh
+++ trunk/build_results.sh
@@ -13,11 +13,11 @@ value=$3
 target_file=${project_path}/report-${value}/children-${report}.csv
 
 head=$(head -1 $project_path/report-${value}/item-${report}.csv)
-echo "Key,${head}" > $target_file
+echo "Key,${head}" 
 for folder in $(find $project_path -name "report-${value}" -type d -maxdepth 2 -mindepth 2) 
 do
 	itemfolder=$(dirname $folder)
 	key=$(basename $itemfolder)	
 	prediction=$(tail -1 ${folder}/item-${report}.csv)
-	echo "${key},${prediction}" >> $target_file
+	echo "${key},${prediction}"
 done
