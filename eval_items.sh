095b90e3efca07701ebfd909390bc25562405a09
matteo.redaelli <matteo.redaelli@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2011-01-26 15:39:28 +0000

    removed to many "

--- eval_items.sh
+++ eval_items.sh
@@ -19,7 +19,7 @@ for file in $(find . -name item.Rdata  -maxdepth ${maxdepth} -mindepth 1) ; do
 	cd $script_path
 	file_path=$(dirname $file)
 	new_item_path=$(echo ${item_path}/${file_path} | sed -e 's://:/:g' -e "s:\./::g")
-	all_params="${project_path} ${new_item_path} ${value} \"${params}\""
+	all_params="${project_path} ${new_item_path} ${value} ${params}"
 	echo "Running params ${all_params}"
 	${script_path}/eval_item.sh ${all_params}
 done
