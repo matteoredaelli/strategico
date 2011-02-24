17ccb18f6e306fc93170906801faeea589334073
matteo.redaelli <matteo.redaelli@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2011-02-24 09:22:43 +0000

    show all keys (also empty) in summary

--- eval_ltp.R
+++ eval_ltp.R
@@ -89,7 +89,7 @@ EvalItemDataByValue <- function(project.path, keys, item.data, value, output.pat
     }
   ## create a single-line summary with short summary (to be merged in report-summary.csv or in the DB, see below)
   if(("summary_db"%in%CONFIG$save) | ("summary_csv"%in%CONFIG$save)) {
-    onerow.summ = onerow.summary(keys, model)
+    onerow.summ = onerow.summary(fullkeys, model)
   }
   if("summary_csv"%in%CONFIG$save) {
     write.table(file = paste(output.path, "/item-summary.csv", sep = ""), onerow.summ, sep = ",", row.names = FALSE, quote = FALSE, col.names = FALSE)
