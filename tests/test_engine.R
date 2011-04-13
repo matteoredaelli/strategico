e464fae28887e9b7e39f2eac1798544876229e50
matteo.redaelli <matteo.redaelli@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2011-04-13 19:55:37 +0000

    more unit tests

--- tests/test_engine.R
+++ tests/test_engine.R
@@ -1,3 +1,41 @@
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## Authors: L. Finos, M. Redaelli
+
+test.GetFields <- function() {
+  checkEquals(
+              c("KEY1", "KEY2"),
+              .GetFields(c("KEY1", "KEY2", "VALUE1", "PERIOD"), "KEY")
+              )
+}
+
+test.GetFieldsId <- function() {
+  checkEquals(
+              c(1,2),
+              .GetFieldsId(c("KEY1", "KEY2", "VALUE1", "PERIOD"), "KEY")
+              )
+}
+test.incSampleTime <- function() {
+  checkEquals(
+              c(2002,1),
+              .incSampleTime(c(2001,2), period.freq = 3, increment = 2)
+              )
+  checkEquals(
+              c(2014,1),
+              .incSampleTime(c(2012,2), period.freq = 2, increment = 3)
+              )
+}
 
 test.BuildFilterWithKeys <- function() {
    checkEquals(
@@ -6,6 +44,13 @@ test.BuildFilterWithKeys <- function() {
                )
 }
 
+test.BuildPeriodRange <- function() {
+  checkEquals(
+              c("2001-2","2001-3","2002-1","2002-2","2002-3"),
+              BuildPeriodRange(c(2001,2), 3, 5)
+              )
+  
+}
 
 test.BuildSQLstmtDeleteRecordsWithKeys <- function() {
   checkEquals(
