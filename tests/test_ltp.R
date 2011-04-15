c198f3a33a6716d5b65dc7be71f5dd02e8262d68
matteo.redaelli <matteo.redaelli@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2011-04-15 19:16:06 +0000

    added more runit tests

--- tests/test_ltp.R
+++ tests/test_ltp.R
@@ -17,7 +17,8 @@ source("eval_ltp.R")
 
 test.EvalItemValue <- function() {
   project.path <- "projects/sample"
-  
+  c <- GetProjectConfig("projects/sample/project.config")
+
   e1     <- EvalItemValue(project.path, keys=c("IT"), value="VALUE1")
   e1.bis <- EvalItemFromProjectData(project.path, keys=c("IT"), value="VALUE1")
   checkEquals(
@@ -28,15 +29,34 @@ test.EvalItemValue <- function() {
               c(5192, 1345, 5824, 1977, 6456, 2609, 7088, 3241),
               as.vector(e1[1,])
               )
+  checkEquals(
+              length( colnames(e1)),
+              c$param$n.ahead
+              )
+  checkEquals(
+              c("2011-1", "2011-2", "2012-1", "2012-2", "2013-1", "2013-2", "2014-1", "2014-2"),
+              colnames(e1)
+              )
+  
+  e11     <- EvalItemValue(project.path, keys=c("IT"), value="VALUE2")
+  e11.bis <- EvalItemFromProjectData(project.path, keys=c("IT"), value="VALUE2")
+  checkEquals(
+              e11,
+              e11.bis
+              )
+  checkEquals(
+              c(13553, 12228, 16187, 20338, 20887, 20824, 25595, 28372),
+              as.vector(e11[1,])
+              )
 
-  e2     <- EvalItemValue(project.path, keys=c("IT", "CAR"), value="VALUE2")
-  e2.bis <- EvalItemFromProjectData(project.path, c("IT", "CAR"), value="VALUE2")
+  e2     <- EvalItemValue(project.path, keys=c("IT", "CAR"), value="VALUE1")
+  e2.bis <- EvalItemFromProjectData(project.path, c("IT", "CAR"), value="VALUE1")
   checkEquals(
               e2,
               e2.bis
               )
   checkEquals(
-              c(6847, 7388, 6895, 7436, 6942, 7483, 6990, 7531),
+              c(5278, 1378, 5996, 2096, 6714, 2814, 7432, 3532),
               as.vector(e2[1,])
               )
   checkEquals(
@@ -44,9 +64,34 @@ test.EvalItemValue <- function() {
               colnames(e2)
               )
 
+  e22     <- EvalItemValue(project.path, keys=c("IT", "CAR"), value="VALUE2")
+  e22.bis <- EvalItemFromProjectData(project.path, c("IT", "CAR"), value="VALUE2")
+  checkEquals(
+              e22,
+              e22.bis
+              )
+  checkEquals(
+              c(6847, 7388, 6895, 7436, 6942, 7483, 6990, 7531),
+              as.vector(e22[1,])
+              )
+  checkEquals(
+              c("2011-1", "2011-2", "2012-1", "2012-2", "2013-1", "2013-2", "2014-1", "2014-2"),
+              colnames(e22)
+              )
   
   ## aggiungere qui' delle chiamate dirette alla funzione ltp()
   ## item.data = ...
   ## model = ltp(...)
   ## e fare qualche checkEquals tra i valori di model
 }
+
+test.EvalItemFromProjectData <- function() {
+  project.path <- "projects/sample"
+  
+  e1 <- EvalItemFromProjectData(project.path, keys=c("","CAR"), value="VALUE1")
+  checkEquals(
+              c(4054, 1915, 3882, 2437, 3949, 2802, 4118, 3100),
+              as.vector(e1[1,])
+              )
+}
+  
