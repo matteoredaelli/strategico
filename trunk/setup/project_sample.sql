7c535695877563aad163bed893415ef342db5588
matteo.redaelli <matteo.redaelli@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2011-09-01 19:18:31 +0000

    fix in sql script

--- trunk/setup/project_sample.sql
+++ trunk/setup/project_sample.sql
@@ -26,8 +26,8 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `sample_data_norm_V1` (
-  `item_id` int(11) NOT NULL default '0',
-  `PERIOD` varchar(20) NOT NULL default '',
+  `item_id` int(11) NOT NULL,
+  `PERIOD` varchar(20) NOT NULL,
   `V` double default NULL,
   PRIMARY KEY  (`item_id`,`PERIOD`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -39,8 +39,8 @@ CREATE TABLE IF NOT EXISTS `sample_data_norm_V1` (
 --
 
 CREATE TABLE IF NOT EXISTS `sample_data_norm_V2` (
-  `item_id` int(11) NOT NULL default '0',
-  `PERIOD` varchar(20) NOT NULL default '',
+  `item_id` int(11) NOT NULL,
+  `PERIOD` varchar(20) NOT NULL,
   `V` double default NULL,
   PRIMARY KEY  (`item_id`,`PERIOD`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -56,8 +56,8 @@ CREATE TABLE IF NOT EXISTS `sample_data_raw` (
   `KEY2` varchar(20) NOT NULL default '',
   `KEY3` varchar(20) NOT NULL default '',
   `PERIOD` varchar(20) NOT NULL default '',
-  `V1` int(11) default NULL,
-  `V2` int(11) default NULL,
+  `V1` double default NULL,
+  `V2` double default NULL,
   PRIMARY KEY  (`KEY1`,`KEY2`,`KEY3`,`PERIOD`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `sample_data_raw` (
 --
 
 CREATE TABLE IF NOT EXISTS `sample_items` (
-  `id` varchar(20) NOT NULL,
+  `id` int(11) NOT NULL,
   `KEY1` varchar(20) default NULL,
   `KEY2` varchar(20) default NULL,
   `KEY3` varchar(20) default NULL,
@@ -83,10 +83,10 @@ CREATE TABLE IF NOT EXISTS `sample_items` (
 --
 
 CREATE TABLE IF NOT EXISTS `sample_results_V1` (
-  `item_id` varchar(20) NOT NULL default '',
+  `item_id` int(11) NOT NULL,
   `model` varchar(20) NOT NULL default '',
   `PERIOD` varchar(20) NOT NULL default '',
-  `V` varchar(20) default NULL,
+  `V` double default NULL,
   PRIMARY KEY  (`item_id`,`model`,`PERIOD`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -97,10 +97,10 @@ CREATE TABLE IF NOT EXISTS `sample_results_V1` (
 --
 
 CREATE TABLE IF NOT EXISTS `sample_results_V2` (
-  `item_id` varchar(20) NOT NULL default '',
+  `item_id` int(11) NOT NULL,
   `model` varchar(20) NOT NULL default '',
   `PERIOD` varchar(20) NOT NULL default '',
-  `V` varchar(20) default NULL,
+  `V` double default NULL,
   PRIMARY KEY  (`item_id`,`model`,`PERIOD`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -111,8 +111,8 @@ CREATE TABLE IF NOT EXISTS `sample_results_V2` (
 --
 
 CREATE TABLE IF NOT EXISTS `sample_summary_models_V1` (
-  `item_id` int(11) NOT NULL default '0',
-  `model` varchar(20) NOT NULL default '',
+  `item_id` int(11) NOT NULL,
+  `model` varchar(20) NOT NULL,
   `formula` varchar(20) default NULL,
   `R2` varchar(20) default NULL,
   `AIC` varchar(20) default NULL,
@@ -129,8 +129,8 @@ CREATE TABLE IF NOT EXISTS `sample_summary_models_V1` (
 --
 
 CREATE TABLE IF NOT EXISTS `sample_summary_models_V2` (
-  `item_id` int(11) NOT NULL default '0',
-  `model` varchar(20) NOT NULL default '',
+  `item_id` int(11) NOT NULL,
+  `model` varchar(20) NOT NULL,
   `formula` varchar(20) default NULL,
   `R2` varchar(20) default NULL,
   `AIC` varchar(20) default NULL,
@@ -147,7 +147,7 @@ CREATE TABLE IF NOT EXISTS `sample_summary_models_V2` (
 --
 
 CREATE TABLE IF NOT EXISTS `sample_summary_V1` (
-  `id` varchar(20) NOT NULL,
+  `id` int(11) NOT NULL,
   `BestModel` varchar(20) default NULL,
   `ICwidth` double default NULL,
   `Points` int(11) default NULL,
@@ -163,8 +163,8 @@ CREATE TABLE IF NOT EXISTS `sample_summary_V1` (
   `SuggestedModel` varchar(20) default NULL,
   `TotModels` int(11) default NULL,
   `Parameters` varchar(1000) default NULL,
-  `ReturnCode` double default NULL,
-  `Run` double default NULL,
+  `ReturnCode` int(11) default NULL,
+  `Run` int(11) default NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -175,7 +175,7 @@ CREATE TABLE IF NOT EXISTS `sample_summary_V1` (
 --
 
 CREATE TABLE IF NOT EXISTS `sample_summary_V2` (
-  `id` varchar(20) NOT NULL,
+  `id` int(11) NOT NULL,
   `BestModel` varchar(20) default NULL,
   `ICwidth` double default NULL,
   `Points` int(11) default NULL,
@@ -191,7 +191,7 @@ CREATE TABLE IF NOT EXISTS `sample_summary_V2` (
   `SuggestedModel` varchar(20) default NULL,
   `TotModels` int(11) default NULL,
   `Parameters` varchar(1000) default NULL,
-  `ReturnCode` double default NULL,
-  `Run` double default NULL,
+  `ReturnCode` int(11) default NULL,
+  `Run` int(11) default NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
