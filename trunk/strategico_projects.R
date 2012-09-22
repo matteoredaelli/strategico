0cd5894818a4449790f1da6b5cc9f03384e70d24
matteo.redaelli@gmail.com <matteo.redaelli@gmail.com@6fe1c6f0-9f5f-f3d5-801c-8cbb03a8c3a4>
2012-09-22 11:40:54 +0000

    added GeProjects function

---
+++ trunk/strategico_projects.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+## This program is fre esoftware: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## Authors: L. Finos, M. Redaelli
+
+## project name: strategico
+## project website: http://code.google.com/p/strategico/
+## created: 2012
+
+Projects.GetProjects <- function(db.channel) {
+  sql <- "select name from strategico_projects order by name"
+  logwarn("Retreiving projects list")
+  DB.RunSQLQuery(sql_statement=sql, db.channel=db.channel) 
+}
